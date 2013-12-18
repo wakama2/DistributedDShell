38163df4cd065723d6a2cad1e4608b09bd8b322e
wakamatsu <yuuki.wakamatsu@gmail.com>
2013-12-18 20:42:19 +0900

    fix broadcast inputstream

--- src/org/GreenTeaScript/D2Shell/Host.java
+++ src/org/GreenTeaScript/D2Shell/Host.java
@@ -11,10 +11,6 @@ import javax.net.SocketFactory;
 
 public abstract class Host {
 	
-//	public Result exec(Request req) {
-//		return exec(req, null, null, null);//FIXME
-//	}
-	
 	public abstract Result exec(Request req, InputStream stdin, OutputStream stdout, OutputStream stderr);
 	
 	public static Host create(String addr, int port) {
@@ -71,6 +67,83 @@ class RemoteHost extends Host {
 	}
 }
 
+class MultiInputStream {
+	private final InputStream in;
+	private final InputStream[] in2;
+	private final DataBlock[] ident;
+	private final int[] idseek;
+	private DataBlock last;
+
+	static class DataBlock {
+		public final byte[] buf = new byte[256];
+		public int size;
+		public DataBlock next;
+	}
+
+	class SubInputStream extends InputStream {
+		private final int id;
+		public SubInputStream(int id) {
+			this.id = id;
+		}
+		public int read() throws IOException {
+			MultiInputStream self = MultiInputStream.this;
+			return self.read(id);
+		}
+	}
+
+	public MultiInputStream(InputStream in, int n) throws IOException {
+		this.in = in;
+		this.in2 = new InputStream[n];
+		this.ident = new DataBlock[n];
+		this.idseek = new int[n];
+		this.last = new DataBlock();
+		for(int i=0; i<n; i++) {
+			this.ident[i] = this.last;
+			this.idseek[i] = 0;
+			this.in2[i] = new SubInputStream(i);
+		}
+		this.last.size = in.read(this.last.buf);
+	}
+
+	public InputStream get(int id) {
+		return this.in2[id];
+	}
+
+	private void readNextBlock() throws IOException {
+		synchronized(this) {
+			if(this.last.next == null) {
+				DataBlock e = new DataBlock();
+				e.size = this.in.read(e.buf);
+				if(e.size != -1) {
+					this.last.next = e;
+					this.last = e;
+				}
+			}
+		}
+	}
+
+	private int read(int id) throws IOException {
+		DataBlock e = ident[id];
+		int seek = idseek[id];
+		if(seek < e.size) {
+			idseek[id] += 1;
+			return e.buf[seek];
+		} else {
+			if(e.next != null) {
+				ident[id] = e.next;
+				idseek[id] = 0;
+				return read(id);
+			} else {
+				readNextBlock();
+				if(e.next == null) {
+					return -1;
+				}
+				return read(id);
+			}
+		}
+	}
+}
+
 class HostGroup extends Host {
 	private final Host[] hosts;
 	
@@ -79,15 +152,35 @@ class HostGroup extends Host {
 	}
 	
 	@Override
-	public Result exec(Request req, InputStream stdin, OutputStream stdout, OutputStream stderr) {
-		Result res = null;
-		String out = "";
-		for(Host host : hosts) {
-			res = host.exec(req, stdin, stdout, stderr);
-			out += res.out;
+	public Result exec(final Request req, InputStream stdin, final OutputStream stdout, final OutputStream stderr) {
+		try {
+			MultiInputStream in = new MultiInputStream(stdin, hosts.length);
+			Thread[] threads = new Thread[hosts.length];
+			final Result[] results = new Result[hosts.length];
+			for(int i=0; i<hosts.length; i++) {
+				final int id = i;
+				final Host host = hosts[i];
+				final InputStream is = in.get(i);
+				threads[i] = new Thread() {
+					public void run() {
+						results[id] = host.exec(req, is, stdout, stderr);
+					}
+				};
+				threads[i].start();
+			}
+			
+			for(int i=0; i<hosts.length; i++) {
+				try {
+					threads[i].join();
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			return Result.merge(results);
+		} catch(IOException e) {
+			e.printStackTrace();
+			return null;
 		}
-		res.out = out;
-		return res;
 	}
 }
 
--- src/org/GreenTeaScript/D2Shell/Result.java
+++ src/org/GreenTeaScript/D2Shell/Result.java
@@ -1,8 +1,10 @@
 package org.GreenTeaScript.D2Shell;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import org.GreenTeaScript.DShell.DShellException;
+import org.GreenTeaScript.DShell.MultipleException;
 
 public class Result implements Serializable {
 	private static final long serialVersionUID = 200L;
@@ -13,5 +15,25 @@ public class Result implements Serializable {
 	public Result(Object out, DShellException exception) {
 		this.out = out;
 		this.exception = exception;
+	}
+	
+	public static Result merge(Result[] res) {
+		Object[] outs = new Object[res.length];
+		ArrayList<DShellException> elist = new ArrayList<>();
+		for(int i=0; i<res.length; i++) {
+			outs[i] = res[i].out;
+			if(res[i].exception != null) {
+				elist.add(res[i].exception);
+			}
+		}
+		DShellException e;
+		if(elist.size() == 0) {
+			e = null;
+		} else if(elist.size() == 1) {
+			e = elist.get(0);
+		} else {
+			e = new MultipleException("", elist.toArray(new DShellException[0]));
+		}
+		return new Result(outs, e);
 	}
 }
